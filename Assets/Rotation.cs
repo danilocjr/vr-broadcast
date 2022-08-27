a552d335a84010acab9d65d655c3cb916146f4bc
Danilo Castro <danilo.castro@compasso.com.br>
2022-08-27 01:47:16 -0300

    Initial Commit

---
+++ Assets/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * 100f);
+        transform.Rotate(Vector3.left, Time.deltaTime * 50f);
+
+    }
+}
